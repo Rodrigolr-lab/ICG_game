8304c67d1c6372d09d086e7e06fdbbabd1f56546
Rodrigolr-lab <rlr@ua.pt>
2024-06-04 03:53:27 +0100

    again 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes, createBrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Game from './components/Game'
 import Memu from './components/Memu'
 
+
 const App = () => {
     return (
         <main className='bg-slate-300/20'>
-            <BrowserRouter basename="/ICG_game">
-                {/* Routes configuration */}
-
-                <Router>
-                    {/* <Navbar /> */}
-                    <Routes>
-                        <Route path="/" element={<Memu />} />
-                        <Route path="/play" element={<Game />} />
-                        <Route path="/projects" element={'projects'} />
-                        <Route path="/contact" element={'contact'} />
-                    </Routes>
-                </Router>
-            </BrowserRouter>
+            <Router>
+                {/* <Navbar /> */}
+                <Routes>
+                    <Route path="/" element={<Memu />} />
+                    <Route path="/play" element={<Game />} />
+                    <Route path="/projects" element={'projects'} />
+                    <Route path="/contact" element={'contact'} />
+                </Routes>
+            </Router>
         </main>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,27 @@
-import ReactDOM from 'react-dom/client' ;
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Route, BrowserRouter as Router, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Game from './components/Game'
+import Memu from './components/Memu'
+
+const router = createBrowserRouter([
+    {
+        path: "/ICG_game/",
+        element: <Memu />,
+        children: [
+            {
+                path: "/ICG_game/",
+                element: <Memu />,
+            },
+            {
+                path: "/ICG_game/play",
+                element: <Game />,
+            },
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
