95fee2fec43e573ce722e58dfda0d9fe558a2f5b
Rodrigolr-lab <rlr@ua.pt>
2024-06-04 03:58:08 +0100

    jjj

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import wasm from 'vite-plugin-wasm'
 export default defineConfig({
   plugins: [react(), wasm()],
   assetsInclude: ['**/*.hdr'], // Add this line to include .hdr files as assets
-  // base: '/ICG_game/', // Replace 'your-repo-name' with the name of your GitHub repository
+  base: '/ICG_game/', // Replace 'your-repo-name' with the name of your GitHub repository
 
 })
